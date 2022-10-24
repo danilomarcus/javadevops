cda7c4587821e9506272e2215f1b2f909cd873f8
Danilo Marcus <danpayne16@msn.com>
2022-10-23 23:35:30 -0300

    commit para artifats

--- src/aplicacao1/Aplicacao1.java
+++ src/aplicacao1/Aplicacao1.java
@@ -4,7 +4,7 @@ public class Aplicacao1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Aplicao 1 tudo OK");
+        System.out.println("Aplicao 1 tudo OK ");
 
     }
 
